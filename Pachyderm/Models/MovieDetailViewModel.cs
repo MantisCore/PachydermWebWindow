43c33cd845c36775500a7b96da8c916805f9acf3
Terje Erwing Bjelkholm <mantiscore@outlook.com>
2020-04-30 22:36:19 +0200

    Render modals with view model parameter

---
+++ Pachyderm/Models/MovieDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pachyderm.Models
+{
+    public class MovieDetailViewModel
+    {
+        public MovieModel Movie { get; set; }
+    }
+}
--- Pachyderm/Services/UI/ModalService.cs
+++ Pachyderm/Services/UI/ModalService.cs
@@ -10,14 +10,19 @@ namespace Pachyderm.Services.UI
 		public event Action<string, RenderFragment> OnShow;
 		public event Action OnClose;
 
-		public void Show(string title, Type contentType)
+		public void Show(string title, Type contentType, object model = null)
 		{
 			if (contentType.BaseType != typeof(ComponentBase))
 			{
 				throw new ArgumentException($"{contentType.FullName} must be a Blazor Component");
 			}
 
-			var content = new RenderFragment(x => { x.OpenComponent(1, contentType); x.CloseComponent(); });
+			var content = new RenderFragment(x => { 
+				x.OpenComponent(0, contentType);
+				if (model != null)
+					x.AddAttribute(1, "Model", model);
+				x.CloseComponent();
+			});
 			OnShow?.Invoke(title, content);
 		}
 
